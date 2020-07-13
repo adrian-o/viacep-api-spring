9ca419e08aa30bfe04b11b3a2484bae0b3b7890e
Adriano Ohana <adriano.ohana@gmail.com>
2020-07-13 18:53:21 -0300

    Primeiro commit

---
+++ src/main/java/com/mrohana/viacep/ViacepApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mrohana.viacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViacepApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViacepApiSpringApplication.class, args);
+	}
+
+}
