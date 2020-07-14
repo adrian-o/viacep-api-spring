3907de56a566940f99d743caacd58a2da9dce280
Adriano Ohana <adriano.ohana@gmail.com>
2020-07-14 18:33:10 -0300

    Alterando URL do Mongo Atlas para o properties e criando prop com a URL do ViaCep

--- src/main/java/com/mrohana/viacep/service/CepService.java
+++ src/main/java/com/mrohana/viacep/service/CepService.java
@@ -8,6 +8,7 @@ import com.mrohana.viacep.data.ViaCepResponse;
 import com.mrohana.viacep.repository.ViaCepRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,14 +18,16 @@ public class CepService {
     @Autowired
     private ViaCepRepository viaCepRepository;
 
+    @Value( "${viacep.client.url}" )
+    private String viaCepUrl;    
+
     public ViaCepResponse getClientViaCep(ViaCepRequest request) {
         ViaCepResponse response = new ViaCepResponse(); 
         response.setRequest(request);
         response.setData(new Date());
         
-        final String uri = "https://viacep.com.br/ws/{cep}/json/";
         RestTemplate restTemplate = new RestTemplate();
-        Endereco endereco = restTemplate.getForObject(uri, Endereco.class, request.getCep());
+        Endereco endereco = restTemplate.getForObject(viaCepUrl, Endereco.class, request.getCep());
         response.setResult(endereco!=null && endereco.getCep()!=null ? 1 : 0);
         response.setEndereco(endereco);
         response.setRequest(request);
